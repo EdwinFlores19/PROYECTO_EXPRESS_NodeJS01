1273690d0106a4d4cfef80a288aca79404f3f3de
EdwinFlores19 <ejuniorfloress@gmail.com>
2023-12-27 02:58:57 -0500

    first commit

---
+++ datos/cursos.js
@@ -0,0 +1,44 @@
+let infoCursos = {
+
+    programacion: [
+        {
+            id: 1,
+            titulo: 'Aprende Python',
+            lenguaje: 'python',
+            vistas: 15000,
+            nivel: 'basico'
+        },
+        {
+            id: 2,
+            titulo: 'Python intermedio',
+            lenguaje: 'python',
+            vistas: 13000,
+            nivel: 'intermedio'
+        },
+        {
+            id: 3,
+            titulo: 'Aprende Javascript',
+            lenguaje: 'javascript',
+            vistas: 120000,
+            nivel: 'basico'
+        }
+    ],
+    matematicas: [
+        {
+            id: 1,
+            titulo: 'Aprende Calculo',
+            tema: 'calculo',
+            vistas: 12457,
+            nivel: 'basico'
+        },
+        {
+            id: 2,
+            titulo: 'Aprende Algebra',
+            tema: 'algebra',
+            vistas: 15455,
+            nivel: 'intermedio'
+        }
+    ]
+}
+
+module.exports.infoCursos = infoCursos;
